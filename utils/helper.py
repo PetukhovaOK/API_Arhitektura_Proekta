ac0dc4fe74304079e2f5439a39edac7506a53ee3
PetukhovaOK <olesya.msk@yandex.ru>
2025-12-15 16:57:40 +0300

    API_Архитектура проекта

--- utils/helper.py
+++ utils/helper.py
@@ -21,12 +21,12 @@ class Helper:
         status_code: int = 200,
         expected_success: bool = True
     ):
-        self.attach_response(response.json())
-        if expected_success:
-            assert response.status_code == status_code, response.json()
-            if isinstance(response.json(), dict):
-                return model(**response.json())
-            elif isinstance(response.json(), list):
-                return [model(**item) for item in response.json()]
-        else:
-            assert response.status_code !=200, response.json()
+        return self.attach_response(response.json())
+    if expected_success:
+        assert response.status_code == status_code, response.json()
+        if isinstance(response.json(), dict):
+            return model(**response.json())
+        elif isinstance(response.json(), list):
+            return [model(**item) for item in response.json()]
+    else:
+        assert response.status_code !=200, response.json()
